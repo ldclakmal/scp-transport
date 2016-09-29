9408e35fc458f93830d31db8e092dfdf69efb7f6
Chanaka Lakmal <ldclakmal@gmail.com>
2016-09-29 10:45:56 +0530

    Implemented SSH commands executing

---
+++ src/main/java/SSHCommand.java
@@ -0,0 +1,31 @@
+import org.junit.Assert;
+
+/**
+ * @author Chanaka Lakmal
+ */
+public class SSHCommand {
+    private static String remoteA = "/tmp/scp/remote-a/abc.txt";
+    private static String remoteB = "/tmp/scp/remote-b/";
+    private static String local = "/tmp/scp/local/";
+
+    private static String userName = "chanaka";
+    private static String host = "192.168.1.143";
+    private static String keyFilePath = "/home/chanaka/.ssh/id_rsa";
+    private static String keyPassword = null;
+    private static int timeOut = 60000;
+
+    public static void main(String[] args) {
+        try {
+            String command = "ls /tmp/scp/remote-a/";
+            SSHManager instance = new SSHManager(userName, host, 22, keyFilePath, keyPassword, timeOut);
+            String errorMessage = instance.connect();
+
+            String expResult = "abc.txt\n";
+            String result = instance.sendCommand(command);
+            instance.close();
+            Assert.assertEquals(expResult, result);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
---
+++ src/main/java/SSHManager.java
@@ -0,0 +1,85 @@
+import com.jcraft.jsch.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author Chanaka Lakmal
+ */
+public class SSHManager {
+    private JSch jschSSHChannel;
+    private String userName;
+    private String host;
+    private int port;
+    private int timeOut;
+    private Session sesConnection;
+
+    public SSHManager(String userName, String host, int port, String keyFilePath,
+                      String keyPassword, int timeOut) {
+
+        this.userName = userName;
+        this.host = host;
+        this.port = port;
+        this.timeOut = timeOut;
+
+        jschSSHChannel = new JSch();
+
+        try {
+            if (keyFilePath != null) {
+                if (keyPassword != null) {
+                    jschSSHChannel.addIdentity(keyFilePath, keyPassword);
+                } else {
+                    jschSSHChannel.addIdentity(keyFilePath);
+                }
+            }
+        } catch (JSchException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String connect() {
+        String errorMessage = null;
+
+        try {
+            sesConnection = jschSSHChannel.getSession(userName, host, port);
+            sesConnection.setConfig("StrictHostKeyChecking", "no");
+            sesConnection.connect(timeOut);
+        } catch (JSchException jschX) {
+            errorMessage = jschX.getMessage();
+        }
+
+        return errorMessage;
+    }
+
+    public String sendCommand(String command) {
+        StringBuilder outputBuffer = new StringBuilder();
+
+        try {
+            Channel channel = sesConnection.openChannel("exec");
+            ((ChannelExec) channel).setCommand(command);
+            InputStream commandOutput = channel.getInputStream();
+            channel.connect();
+            int readByte = commandOutput.read();
+
+            while (readByte != 0xffffffff) {
+                outputBuffer.append((char) readByte);
+                readByte = commandOutput.read();
+            }
+
+            channel.disconnect();
+        } catch (IOException ioX) {
+            ioX.printStackTrace();
+            return null;
+        } catch (JSchException jschX) {
+            jschX.printStackTrace();
+            return null;
+        }
+
+        return outputBuffer.toString();
+    }
+
+    public void close() {
+        sesConnection.disconnect();
+    }
+
+}
